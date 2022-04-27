edcef51f83bb905574d781de156ae4d264e41bed
Vladimir Mikhalenko <mikhalenko@acdlabs.by>
2022-04-27 11:25:25 +0300

    fix: arrows markers is missing in some cases

--- dev/raphael.svg.js
+++ dev/raphael.svg.js
@@ -215,7 +215,7 @@ define(["./raphael.core"], function(R) {
                 o._.arrows = {};
             }
             if (type != "none") {
-                var pathId = "raphael-marker-" + type,
+                var pathId = "raphael-marker-" + type + "-obj" + p.id,
                     markerId = "raphael-marker-" + se + type + w + h + "-obj" + o.id;
                 if (!R._g.doc.getElementById(pathId)) {
                     p.defs.appendChild($($("path"), {
